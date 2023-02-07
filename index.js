054b2d1a7cee041caf743147e074eb5d7c250ac1
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-08 01:02:13 +0900

    config express 2

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require('dotenv').config()
 const express = require('express')
+const { getLogger } = require('nodemailer/lib/shared')
 
 const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => res.send('Apps worked successfully'))
 
+app.use((err, req, res, next) => {
+  if (res.headersSend) return next(err)
+  res.status(400).json({ message: err.message })
+})
+
+const PORT = process.env.PORT || 5055
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
