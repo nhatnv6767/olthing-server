c9868ba7561c52d0a4b3141457e53d0fdf657974
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-08 01:08:42 +0900

    connect db

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
 const express = require('express')
-const { getLogger } = require('nodemailer/lib/shared')
 
+const ConnectDb = require('./config/db')
+
+// app initialization
 const app = express()
+
+// middleware
 app.use(express.json())
+
+// connect db
+ConnectDb()
 
 app.get('/', (req, res) => res.send('Apps worked successfully'))
 
