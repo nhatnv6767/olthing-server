2ac682fa731784cbf30b505766a1695d035be8f1
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-08 00:53:32 +0900

    ..2

---
+++ index.js
@@ -0,0 +1,41 @@
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+
+var index = express()
+
+// view engine setup
+index.set('views', path.join(__dirname, 'views'))
+index.set('view engine', 'pug')
+
+index.use(logger('dev'))
+index.use(express.json())
+index.use(express.urlencoded({ extended: false }))
+index.use(cookieParser())
+index.use(express.static(path.join(__dirname, 'public')))
+
+index.use('/', indexRouter)
+index.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+index.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+index.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = index
