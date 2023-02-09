c8e6d21a10f77da28dd7437cdef991773c9c9011
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-09 23:19:56 +0900

    db create User 2

--- models/User.js
+++ models/User.js
@@ -91,5 +91,15 @@ userSchema.methods.comparePassword = function (password, hash) {
 
 // generateConfirmationToken
 userSchema.methods.generateConfirmationToken = function () {
+  const token = crypto.randomBytes(32).toString('hex')
+  this.confirmationToken = token
+  const date = new Date()
 
+  date.setDate(date.getDate() + 1)
+  this.confirmationTokenExpires = date
+
+  return token
 }
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
