1aad5ad04745f540b2fc58d971a5cc0224788596
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-10 23:46:16 +0900

    validator login

--- utils/validators.js
+++ utils/validators.js
@@ -30,3 +30,19 @@ module.exports.validateRegisterInput = (
   }
 
 }
+
+module.exports.validateLoginInput = (email, password) => {
+  const errors = {}
+
+  if (email.trim() === "") {
+    errors.email = 'Email không được để trống'
+  }
+  if (password.trim() === "") {
+    errors.password = 'Mật khẩu không được để trống'
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  }
+}
