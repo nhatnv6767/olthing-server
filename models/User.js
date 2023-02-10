3ec377638a126d60351e8e9ffee4e104e1d23517
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-10 22:46:23 +0900

    db create User 5 fix

--- models/User.js
+++ models/User.js
@@ -1,28 +1,10 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 const crypto = require('crypto')
 const bcrypt = require('bcryptjs')
+const Schema = mongoose.Schema
 
-const userSchema = mongoose.Schema(
+const UserSchema = new Schema(
   {
-    email: {
-      type: String,
-      validate: [validator.isEmail, 'Vui lòng cung cấp email chính xác'],
-      trim: true,
-      lowercase: true,
-      unique: true,
-      required: [true, 'Cần nhập vào email'],
-    },
-    password: {
-      type: String,
-      required: [true, 'Cần nhập vào mật khẩu'],
-      minLength: [6, 'Mật khẩu cần ít nhất 6 ký tự'],
-    },
-    role: {
-      type: String,
-      enum: ['user', 'admin'],
-      default: 'user',
-    },
     name: {
       type: String,
       required: [true, 'Nhập vào họ tên'],
@@ -30,76 +12,88 @@ const userSchema = mongoose.Schema(
       minLength: [3, 'Độ dài tối thiểu của tên là 3 ký tự'],
       maxLength: [100, 'Tên quá dài'],
     },
-    contactNumber: {
+    username: {
       type: String,
-      validate: [
-        validator.isMobilePhone,
-        'Xin hãy cung cấp số điện thoại chính xác'],
+      required: true,
     },
-    shippingAddress: String,
-    imageURL: {
+    email: {
       type: String,
-      validate: [validator.isURL, 'Cần cung cấp đường dẫn chính xác'],
+      required: true,
+      unique: true,
+      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    newpassword: {
+      type: String,
     },
     phone: {
       type: String,
-      required: false,
+      required: true,
     },
-    address: {
+    profile_picture: {
       type: String,
       required: false,
     },
-    bio: {
+    role: {
+      type: String,
+      required: true,
+    },
+    orders: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+    ],
+    email_confirmation: {
       type: String,
       required: false,
     },
-    status: {
+    reset_password_token: {
       type: String,
-      default: 'inactive',
-      enum: ['active', 'inactive', 'blocked'],
+      required: false,
+    },
+    reset_password_expiration: {
+      type: String,
+      required: false,
     },
 
-    confirmationToken: String,
-    confirmationTokenExpires: Date,
-
-    passwordChangeAt: Date,
-    passwordResetToken: String,
-    passwordResetExpires: Date,
   },
   {
     timestamps: true,
   },
 )
 
-userSchema.pre('save', function (next) {
-  if (!this.isModified('password')) {
-    // only run if password is modified, otherwise it will change every time we save the user!
-    return next()
-  }
+// userSchema.pre('save', function (next) {
+//   if (!this.isModified('password')) {
+//     // only run if password is modified, otherwise it will change every time we save the user!
+//     return next()
+//   }
+//
+//   const password = this.password
+//   const hashedPassword = bcrypt.hashSync(password)
+//   this.password = hashedPassword
+//   next()
+// })
+//
+// // comparePassword
+// userSchema.methods.comparePassword = function (password, hash) {
+//   const isPasswordValid = bcrypt.compareSync(password, hash)
+//   return isPasswordValid
+// }
+//
+// // generateConfirmationToken
+// userSchema.methods.generateConfirmationToken = function () {
+//   const token = crypto.randomBytes(32).toString('hex')
+//   this.confirmationToken = token
+//   const date = new Date()
+//
+//   date.setDate(date.getDate() + 1)
+//   this.confirmationTokenExpires = date
+//
+//   return token
+// }
 
-  const password = this.password
-  const hashedPassword = bcrypt.hashSync(password)
-  this.password = hashedPassword
-  next()
-})
-
-// comparePassword
-userSchema.methods.comparePassword = function (password, hash) {
-  const isPasswordValid = bcrypt.compareSync(password, hash)
-  return isPasswordValid
-}
-
-// generateConfirmationToken
-userSchema.methods.generateConfirmationToken = function () {
-  const token = crypto.randomBytes(32).toString('hex')
-  this.confirmationToken = token
-  const date = new Date()
-
-  date.setDate(date.getDate() + 1)
-  this.confirmationTokenExpires = date
-
-  return token
-}
-
-const User = mongoose.model('User', userSchema)
-module.exports = User
+module.exports = mongoose.model('User', UserSchema)
