d936d1a763600461fe81f47a19488226a1a52c4f
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-10 23:25:12 +0900

    db schema product

--- models/Product.js
+++ models/Product.js
@@ -1,79 +1,57 @@
 const mongoose = require('mongoose')
-const valid = require('validator')
+const Schema = mongoose.Schema
 
-const productSchema = mongoose.Schema(
-  {
-    sku: {
-      type: String,
-      required: false,
-    },
-    title: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    parent: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    children: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    tags: [String],
-    image: {
-      type: String,
-      required: true,
-      validate: [valid.isURL, 'sai đường dẫn'],
-    },
-    originalPrice: {
-      type: Number,
-      required: true,
-      min: [0, 'Giá không thể là số âm'],
-    },
-    price: {
-      type: Number,
-      required: true,
-      default: 0,
-      min: [0, 'Giá không thể là số âm'],
-    },
-    relatedImages: [
-      {
-        type: String,
-        required: true,
-        validate: [valid.isUrl, 'sai đường dẫn'],
-      }],
-    description: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    brand: {
-      type: String,
-      required: true,
-    },
-    unit: {
-      type: String,
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
-    colors: [String],
+const productSchema = new Schema({
+  name: {
     type: String,
-    itemInfo: String,
-    status: {
-      type: String,
-      default: 'active',
-      enum: ['active', 'inActive'],
-    },
-  }, {
-    timestamps: true,
+    required: true
   },
-)
+  description: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: String,
+    required: true,
+  },
+  image_public_id: {
+    type: String,
+    required: false
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  ratings: {
+    type: String,
+    required: false,
+  },
+  reviews: [
+    {
+      title: String,
+      text: String,
+      rating: String,
+      username: String,
+      createdAt: String,
+    }
+  ],
+  total_in_stock: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: String,
+    required: true
+  }
 
-const Product = mongoose.model('Products', productSchema)
-module.exports = Product
+})
+
+module.exports = mongoose.model('Product', productSchema)
