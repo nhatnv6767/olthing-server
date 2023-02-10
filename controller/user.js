39063be42fe67943dad82ff48dea386648e3743a
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-10 23:58:21 +0900

    controller user Login

--- controller/user.js
+++ controller/user.js
@@ -2,7 +2,49 @@ const bcrypt = require('bcryptjs')
 
 const User = require('../models/User')
 const genAccTkn = require('../helpers/genAccessToken')
-// const {
-//   validateRegisterInput,
-//   validateLoginInput,
-// } = require("../utils/va")
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require('../utils/validators')
+
+exports.postLogin = async (req, res) => {
+  try {
+    const email = req.body.email
+    const password = req.body.password
+
+    const { errors, valid } = validateLoginInput(email, password)
+
+    if (!valid) {
+      return res.status(401).json({
+        errors
+      })
+    }
+
+    const user = await User.findOne({
+      email,
+    })
+    if (!user) {
+      return res.status(401).json({
+        error: "Email or mật khẩu không chính xác"
+      })
+    }
+
+    const isEqual = await bcrypt.compare(password, user.password)
+
+    if (!isEqual) {
+      return res.status(401).json({
+        error: "Email or mật khẩu không chính xác"
+      })
+    }
+
+    const token = genAccTkn.generateAccessToken(user)
+    return res.status(200).json({
+      id: user.id,
+      token,
+      tokenExpiration: "24h"
+    })
+
+  } catch (err) {
+    res.status(500)
+  }
+}
