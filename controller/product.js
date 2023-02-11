2e1ad573b39fd346f233426156d84d5770652a7f
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-11 09:35:51 +0900

    controller product add a product

--- controller/product.js
+++ controller/product.js
@@ -30,3 +30,37 @@ exports.fetchProducts = async (req, res) => {
     res.status(500)
   }
 }
+
+exports.addProduct = async (req, res) => {
+  try {
+    const name = req.body.product_name;
+    const description = req.body.product_description;
+    const type = req.body.product_type;
+    const price = req.body.product_price
+    const color = req.body.product_color
+    const total_in_stock = req.body.total_in_stock
+    const image_public_id = req.body.image_public_id
+    const file = req.files.file;
+
+    const product = new Product({
+      name,
+      description,
+      type,
+      images: file.name,
+      image_public_id,
+      price,
+      color,
+      reviews: [],
+      total_in_stock,
+      createdAt: new Date().toISOString(),
+    })
+
+    await product.save()
+    return res.status(200).json({
+      message: "Đã thêm 1 sản phẩm"
+    })
+
+  } catch (e) {
+    res.status(500)
+  }
+}
